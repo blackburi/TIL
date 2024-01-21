3cd6ae0f7bb08352192b9e59b316e6536faf4428
김태경 <98coding@gmail.com>
2024-01-21 19:16:41 +0900

    solve

--- baekjoon/greedy/1026.py
+++ baekjoon/greedy/1026.py
@@ -1,1 +1,23 @@
 # 보물
+
+
+
+"""시간 초과
+N = int(input())
+
+discuss = []
+
+for _ in range(N) :
+    a, b = list(map(int, input().split()))
+    if len(discuss) < b+1 :
+        for _ in range(b+1-len(discuss)) :
+            discuss.append(0)
+    else :
+        pass
+    
+    for i in range(b+1) :
+        if a < i < b :
+            discuss[i] += 1
+
+print(max(discuss))
+"""
