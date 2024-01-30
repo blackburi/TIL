0aa290d7777234dcab462dbe7aa78f0e9dfb6814
김태경 <aceocs07@gmail.com>
2024-01-30 11:04:49 +0900

    study

--- baekjoon/my_study/silver/s2_17103.py
+++ baekjoon/my_study/silver/s2_17103.py
@@ -1,2 +1,19 @@
-N = int(input())
+T = int(input())
 
+def isprime(x) :
+    for i in range(3, int(x**0.5+2)) :
+        if x == 1 :
+            return False
+        else :
+            if x % i == 0 :
+                return False
+            return True
+
+for t in range(T) :
+    n = int(input())
+    cnt = 0
+    for j in range(3, int(n**0.5+2)) :
+        if isprime(j) is True :
+            if isprime(n-j) is True :
+                cnt += 1
+    print(cnt)
