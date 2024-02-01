34689ae4293c8d786912c9780326482198b911f1
김태경 <98coding@gmail.com>
2024-02-01 17:08:35 +0900

    solve

--- SWEA/SWEA_18062.py
+++ SWEA/SWEA_18062.py
@@ -7,8 +7,35 @@ input = sys.stdin.readline
 T = int(input())
 
 for tc in range(1, T+1) :
-    num = list(map(int, input().split()))
+    n = int(input())
+    num_list = list(map(int, input().split()))
 
-    for i in range(len(num)-1) :
-        for j in range(i+1, len(num)-1) :
-            if 
+    ans = []
+
+    num = deque([])
+
+    while len(num_list) > 0 :
+        if len(num) == 0 :
+            k = num_list.pop()
+            num.append(k)
+        else : # len(num) != 0
+            a = num_list.pop()
+            for i in range(len(num)) :
+                if a < num[i] :
+                    num.insert(i, a)
+                    break
+                elif a > num[-1] :
+                    num.append(a)
+
+    while len(num) > 0 :
+        m = num.pop()
+        ans.append(m)
+        
+        if len(num) != 0 :
+            l = num.popleft()
+            ans.append(l)
+    
+    if len(ans) > 10 :
+        ans = ans[:10]
+
+    print(f'#{tc}', *ans)
