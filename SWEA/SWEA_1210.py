0ab158e8c6523d9e1607bdb0c36ee2741fb9a699
김태경 <98coding@gmail.com>
2024-02-01 17:54:54 +0900

    fix

--- SWEA/SWEA_1210.py
+++ SWEA/SWEA_1210.py
@@ -2,20 +2,18 @@
 
 for tc in range(1, 11) :
     n = int(input())
-    mat = [list(map(int, input().split())) for _ in range(100)]
+    mat = [input().split() for _ in range(100)]
 
     x = 98
     y = mat[99].index(2) # 2의 위치는 mat[99][k]
 
     while x > 0 :
+        mat[x][y] = 0
         if y-1>=0 and mat[x][y-1] == 1 :
-            mat[x][y] = 0
             y -= 1
         elif y+1<=99 and mat[x][y+1] == 1 :
-            mat[x][y] = 0
             y += 1
         elif mat[x-1][y] == 1 :
-            mat[x][y] = 0
             x -= 1
     
     print(f'#{tc} {y}')
