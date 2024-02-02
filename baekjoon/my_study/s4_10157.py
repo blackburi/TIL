cb3355288c87397a9a695724d7d6e29558a59d8d
김태경 <98coding@gmail.com>
2024-02-02 17:19:26 +0900

    solve

--- baekjoon/my_study/s4_10157.py
+++ baekjoon/my_study/s4_10157.py
@@ -4,13 +4,17 @@ number = int(input())
 
 dx = [-1, 0, 1, 0]
 dy = [0, 1, -1, 0]
+m = 0
 
 if number > c * r :
     print(0)
 
-mat = [[0] * r for _ in range(c)]
+mat = [[0] * (r+2) for _ in range(c+2)]
+
+x = r
+y = 1
+mat[r][1] = 1
+number -= 1
 
 while number :
-    mat[r-1][0] = 1
-    number -= 1
-    if 
+    if 1 <= x <= r and mat[x][y] == 0 :
