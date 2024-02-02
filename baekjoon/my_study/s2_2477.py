416a2c0d936641303639e117cd43fa4d345485f6
김태경 <98coding@gmail.com>
2024-02-02 15:59:37 +0900

    solve

--- baekjoon/my_study/s2_2477.py
+++ baekjoon/my_study/s2_2477.py
@@ -3,8 +3,18 @@ n = int(input())
 length = []
 
 
-for i in range(1, 7) :
+for _ in range(6) :
     a, b = map(int, input().split())
-    
-    length.append([i, a, b])
+    length.append(b)
 
+
+k = max(length)
+
+p = length.index(k)
+
+if length[(p+1)%6] > length[(p+5)%6] :
+    area = length[p] * length[(p+1)%6] - length[(p+3)%6] *length[(p+4)%6]
+else : #length[(p+1)%6] < length[(p+5)%6] 같을수는 없음
+    area = length[p] * length[(p+5)%6] - length[(p+3)%6] *length[(p+2)%6]
+
+print(area * n)
