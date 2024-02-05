60720b7d6ae5dd7a2052f7da5b87e0b28e8bc1e9
김태경 <98coding@gmail.com>
2024-02-05 12:27:35 +0900

    solve

--- baekjoon/my_study/silver/s2_1012.py
+++ baekjoon/my_study/silver/s2_1012.py
@@ -10,8 +10,8 @@ for _ in range(T) :
         a, b = map(int, input().split())
         cabbage.append([a, b])
 
-    cabbage.sort(key = lambda x : (x[0], x[1]))
-    
+    cabbage.sort(key = lambda x : (x[1], x[0]))
+
     need = 0
     dum = [] # 붙어있는 배추의 모음
     while cabbage :
@@ -20,7 +20,7 @@ for _ in range(T) :
             need += 1
             dum.append([x, y])
         else : # dum != []
-            dum.append([x, y])
+            cnt = 0
             for i in dum :
                 if [i[0], i[1]+1] in cabbage:
                     dum.append([i[0], i[1]+1])
@@ -30,7 +30,16 @@ for _ in range(T) :
                     dum.append([i[0]+1, i[1]])
                     idx = cabbage.index([i[0]+1, i[1]])
                     del cabbage[idx]
+                elif [i[0], i[1]-1] in cabbage:
+                    dum.append([i[0], i[1]-1])
+                    idx = cabbage.index([i[0], i[1]-1])
+                    del cabbage[idx]
+                elif [i[0]-1, i[1]] in cabbage:
+                    dum.append([i[0]-1, i[1]])
+                    idx = cabbage.index([i[0]-1, i[1]])
+                    del cabbage[idx]
                 else :
-                    if i == dum[-1] :
-                        dum = []
+                    cnt += 1
+            if cnt == len(dum) :
+                dum = []
     print(need)
