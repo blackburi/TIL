afbe6d8eceadd6fe6b2eac8ca702b53317bc85d8
김태경 <98coding@gmail.com>
2024-02-07 14:24:40 +0900

    solve SWEA 2005

--- SWEA/08.Stack1_1/2005.py
+++ SWEA/08.Stack1_1/2005.py
@@ -2,19 +2,20 @@ T = int(input())
 
 for tc in range(1, T+1) :
     n = int(input())
-    price = list(map(int, input().split()))
+    pascal = [[0]*i for i in range(1, n+1)]
 
-    ans = 0
-    stack = []
-
-    stack.append(price.pop())
-
-    while price :
-        a = price.pop()
-        if a <= stack[-1] :
-            ans += stack[-1] - a
-        else : # a > stack[-1]
-            stack.pop()
-            stack.append(a)
+    for k in range(n) :
+        if k == 0 :
+            pascal[k] = [1]
+        elif k == 1 :
+            pascal[k][0] = 1
+            pascal[k][-1] = 1
+        else : # k > 1
+            pascal[k][0] = 1
+            pascal[k][-1] = 1
+            for j in range(1, k) :
+                pascal[k][j] = pascal[k-1][j-1] + pascal[k-1][j]
     
-    print(f'#{tc} {ans}')
+    print(f'#{tc}')
+    for p in range(n) :
+        print(*pascal[p])
