78c1a2b8d9ea112ab4a9b2718975f3dff129395a
김태경 <98coding@gmail.com>
2024-02-07 14:16:01 +0900

    sovle SWEA 1859

---
+++ SWEA/08.Stack1_1/2005.py
@@ -0,0 +1,20 @@
+T = int(input())
+
+for tc in range(1, T+1) :
+    n = int(input())
+    price = list(map(int, input().split()))
+
+    ans = 0
+    stack = []
+
+    stack.append(price.pop())
+
+    while price :
+        a = price.pop()
+        if a <= stack[-1] :
+            ans += stack[-1] - a
+        else : # a > stack[-1]
+            stack.pop()
+            stack.append(a)
+    
+    print(f'#{tc} {ans}')
