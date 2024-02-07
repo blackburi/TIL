cbd5e0c3a94b13ad3194587b10a32d1b44160d9a
김태경 <98coding@gmail.com>
2024-02-07 14:33:13 +0900

    solve SWEA 1234

---
+++ SWEA/08.Stack1_1/1234.py
@@ -0,0 +1,16 @@
+for tc in range(1, 11) :
+    n, sub = list(map(str, input().split()))
+    
+    lst = list(sub)
+    stack = []
+    
+    while lst :
+        if stack == [] or lst[-1] != stack[-1] :
+            stack.append(lst.pop())
+        else : 
+            lst.pop()
+            stack.pop()
+    
+    stack.reverse()
+    print(''.join(stack))
+    print(f'#{tc} {"".join(stack)}')
