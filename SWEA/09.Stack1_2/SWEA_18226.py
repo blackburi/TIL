0cfa79e6939dc2cdd59844e282e45c279aab7db4
김태경 <98coding@gmail.com>
2024-02-08 12:51:17 +0900

    solve SWEA_18226 another solution

--- SWEA/09.Stack1_2/SWEA_18226.py
+++ SWEA/09.Stack1_2/SWEA_18226.py
@@ -1,3 +1,4 @@
+# solution 1 - 점화식으로 풀기
 T = int(input())
 
 for tc in range(1, T+1) :
@@ -13,3 +14,10 @@ for tc in range(1, T+1) :
             return fibo(k-1) + 2 * fibo(k-2)
     
     print(f'#{tc} {fibo(n)}')
+
+# solution 2 - 점화식을 방정식으로 바꿔 풀기
+T = int(input())
+
+for tc in range(1, T+1) :
+    n = int(input())
+    print(f'#{tc} {(2**(n//10+1) + (-1)**(n//10))//3}')
