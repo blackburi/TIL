47d687f932064313e0fd5f6c5005d79036f2b13e
김태경 <98coding@gmail.com>
2024-02-13 17:51:16 +0900

    solve SWEA_18404

--- SWEA/10.stack2_1/SWEA_18404.py
+++ SWEA/10.stack2_1/SWEA_18404.py
@@ -1,7 +1,32 @@
-lst = list(map(int, input().split()))
-cnt = 0
+## 태완풀이
+lst = [i for i in range(1, 11)]
+n = len(lst)
+count = 0
+for i in range(1 << n):
+    total = 0
+    for j in range(n):
+        if i & (1 << j):
+            total += lst[j]
+            if 10 < total:
+                break
+    if total == 10:
+        count += 1
+print(f"#{1} {count}")
 
-# num : 더할 수의 index, total : 수들의 합
-graph = [lst[i+1:10] for i in range(9)]
+## 광영 풀이 - 수정 필요
+def dfs(idx: int, total: int) -> None:
+    global result
+ 
+    if total + nums[idx] > 10 :
+        return
+    elif total + nums[idx] == 10 :
+        result += 1
+        return
+ 
+    for i in range(idx + 1, 10) :
+        dfs(i, total + nums[idx])
 
-print(graph)
+nums = list(range(1, 11))
+result = 0
+dfs(0, 0)
+print(f'#1 {result}')
