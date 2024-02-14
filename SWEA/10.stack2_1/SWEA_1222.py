29480ad092708700aed73d17bc4c1e3c3f087597
김태경 <98coding@gmail.com>
2024-02-14 09:42:37 +0900

    fix SWEA_1222

--- SWEA/10.stack2_1/SWEA_1222.py
+++ SWEA/10.stack2_1/SWEA_1222.py
@@ -14,3 +14,32 @@ for tc in range(1, 11) :
         ans += j
     
     print(f'#{tc} {ans}')
+
+# 후위 표기식
+dum = input() # 중위 표기식으로 된 입력값
+ans = [] # 후위표기식
+stack = [] # 연산자를 넣을 stack
+
+for i in dum :
+    if i not in ['(', ')', '+', '-', '*', '/'] :
+        ans.append(i)
+    else :
+        if i == '(' :
+            stack.append(i)
+        elif i == '*' or i == '/' :
+            while len(stack) > 0 and (stack[-1] == '*' or stack[-1] == '/') :
+                ans.append(stack.pop())
+            stack.append(i)
+        elif i == '+' or i == '-' :
+            while len(stack) > 0 and stack[-1] != '(' :
+                ans.append(stack.pop())
+            stack.append(i)
+        elif i == ')' :
+            while len(stack) > 0 and stack[-1] != '(' :
+                ans.append(stack.pop())
+            stack.pop()
+
+while len(stack) > 0 :
+    ans.append(stack.pop())
+
+print(''.join(ans))
