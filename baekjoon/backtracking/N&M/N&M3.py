bc7e0e87f525b8a320af4faaa603d6a4258145ef
김태경 <98coding@gmail.com>
2024-02-15 00:35:01 +0900

    solving n&m3

--- baekjoon/backtracking/N&M/N&M3.py
+++ baekjoon/backtracking/N&M/N&M3.py
@@ -1,1 +1,12 @@
 # N & M 3
+
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().rstrip().split())
+lst = [i for i in range(1, n+1)]
+
+for _ in range(m) :
+    sub = []
+    while len(sub) < m :
+        
