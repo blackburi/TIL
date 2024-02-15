e9197ab4edf0539e080bd22966f4a2414a6c8c91
김태경 <98coding@gmail.com>
2024-02-15 12:35:12 +0900

    find another solution

--- SWEA/12.Queue1/SWEA_18400.py
+++ SWEA/12.Queue1/SWEA_18400.py
@@ -6,9 +6,8 @@ for tc in range(1, T+1) :
     n, m = map(int, input().split())
     lst = deque(list(map(int, input().split())))
 
-    while m :
-        x = lst.popleft()
-        lst.append(x)
-        m -= 1
+    m = m % n
+    for _ in range(m) :
+        lst.popleft()
     
     print(f'#{tc} {lst[0]}')
