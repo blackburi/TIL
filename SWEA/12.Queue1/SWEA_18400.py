0cb772a316724c57395a1fc394aed9e4cd923a46
김태경 <98coding@gmail.com>
2024-02-15 12:37:06 +0900

    find another solution

--- SWEA/12.Queue1/SWEA_18400.py
+++ SWEA/12.Queue1/SWEA_18400.py
@@ -1,13 +1,9 @@
-from collections import deque
-
 T = int(input())
 
 for tc in range(1, T+1) :
     n, m = map(int, input().split())
-    lst = deque(list(map(int, input().split())))
+    lst = list(map(int, input().split()))
 
     m = m % n
-    for _ in range(m) :
-        lst.popleft()
     
-    print(f'#{tc} {lst[0]}')
+    print(f'#{tc} {lst[m]}')
