e64df922c553a1f966040a95df5c1db43eb2be67
김태경 <98coding@gmail.com>
2024-02-21 16:45:45 +0900

    fail to solve

--- SWEA/14.Tree_1/SWEA_11315.py
+++ SWEA/14.Tree_1/SWEA_11315.py
@@ -2,29 +2,37 @@ T = int(input())
 
 for tc in range(1, T+1) :
     n = int(input())
-    mat = [list(input()) for _ in range(n)]
     
-    # x행을 검사
-    def row(x) :
-        cnt = 0
-        for i in range(n) :
-            if mat[x][i] == 'o' :
-                cnt += 1
-                if cnt == 5 :
-                    break
-            else :
-                cnt = 0
-        if cnt == 5 :
-            return True
-        else :
-            return False
+    # 가로, 우하향 대각선
+    r_check = [[] for _ in range(n)]
+    # 세로, 좌하향 대각선
+    c_check = [[] for _ in range(n)]
+
+    for i in range(n) :
+        lst = list(input())
+        for j in range(n) :
+            if lst[j] == 'o' :
+                r_check[j].append(i)
+                c_check[i].append(j)
     
-    # y열을 검사
-    def col(y) :
-        if ['o', 'o', 'o', 'o', 'o'] in zip(mat[i] for i in range(n)) :
-            return True
-        else :
-            return False
-    
-    # 대각선 검사
-    def cross()
+    cnt = 0
+    for p in range(n-4) :
+        if len(r_check(p)) > 0 and len(r_check(p+1)) > 0 and len(r_check(p+2)) > 0 and len(r_check(p+3)) > 0 and len(r_check(p+4)) > 0 :
+           cnt += 1
+           break
+        if r_check :
+            for i in r_check :
+                if (i+1) in r_check
+
+    if cnt == 1 :
+        print(f'#{tc} YES')
+        continue
+
+    for p in range(n-4) :
+        if len(c_check(p)) > 0 and len(c_check(p+1)) > 0 and len(c_check(p+2)) > 0 and len(c_check(p+3)) > 0 and len(c_check(p+4)) > 0 :
+            cnt += 1
+            break
+
+    if cnt == 1 :
+        print(f'#{tc} YES')
+        continue
