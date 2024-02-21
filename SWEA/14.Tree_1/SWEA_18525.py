7ae52a5fb9dbe54adcddcaf40e7701a360bcfd3d
김태경 <98coding@gmail.com>
2024-02-21 14:32:46 +0900

    solve SWEA_18525.py

--- SWEA/14.Tree_1/SWEA_18525.py
+++ SWEA/14.Tree_1/SWEA_18525.py
@@ -3,15 +3,18 @@ T = int(input())
 for tc in range(1, T+1) :
     n = int(input())
 
-    lst = [0]
-    def inorder(x) :
+    lst = [0] * (n+1)
+
+    def number(x) :
+        global cnt
         if x > n :
             return
-        if x :
-            inorder(2*x)
-            lst.append(x)
-            inorder(2*x+1)
+        number(2*x)
+        lst[x] = cnt
+        cnt += 1
+        number(2*x+1)
+    
+    cnt = 1
+    number(1)
 
-    # ans[a] = b : a번째 노드에는 b가 들어있다.
-    inorder(1)
-    print(lst)
+    print(f'#{tc} {lst[1]} {lst[n//2]}')
