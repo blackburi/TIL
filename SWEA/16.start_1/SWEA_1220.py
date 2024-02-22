ebd1e6ffa8413ef86164195892f2833edf44ef0f
김태경 <98coding@gmail.com>
2024-02-22 11:56:49 +0900

    fix explaining

--- SWEA/16.start_1/SWEA_1220.py
+++ SWEA/16.start_1/SWEA_1220.py
@@ -10,8 +10,10 @@ for tc in range(1, 11) :
                 while 0<=a<=99 :
                     if mat[a][j] == '0' :
                         pass
+                    # 1을 만나면 교차 X
                     elif mat[a][j] == '1' :
                         break
+                    # 2를 만나면 교착 상태 발생
                     else : # mat[a][j] == '2'
                         cnt += 1
                         break
