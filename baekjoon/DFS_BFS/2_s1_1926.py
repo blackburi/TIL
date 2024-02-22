e8cee732ddc8ec315ad30de7ee8744b10aa61aa5
김태경 <98coding@gmail.com>
2024-02-22 15:46:58 +0900

    fail to solve

--- baekjoon/DFS_BFS/2_s1_1926.py
+++ baekjoon/DFS_BFS/2_s1_1926.py
@@ -2,33 +2,10 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-n, m = map(int, input().rstrip().split())
-mat = [list(map(int, input().rstrip().split())) for _ in range(n)]
+n, m = map(int, input().split())
+mat = [list(map(str, input().split()))]
 
 cnt = 0 # 그림의 수
 area = 0 # 그림의 최대 넓이
+tmp = 0
 
-stack = deque([])
-for i in range(n) :
-    for j in range(m) :
-        if mat[i][j] == 1 :
-            stack.append([i, j])
-
-while stack :
-    a, b = stack.popleft()
-    cnt += 1
-    tmp = 0
-    sub = deque([[a, b]])
-    while sub :
-        x, y = sub.popleft()
-        tmp += 1
-        for p, q in [[1, 0], [0, 1], [-1, 0], [0, -1]] :
-            if [x+p, y+q] in stack :
-                stack.remove([x+p, y+q])
-                sub.append([x+p, y+q])
-    
-    if area < tmp :
-        area = tmp
-
-print(cnt)
-print(area)
