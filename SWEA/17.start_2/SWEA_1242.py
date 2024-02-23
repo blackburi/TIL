d3dcb261c7481ac3530f915efee889fa8b2963dd
김태경 <98coding@gmail.com>
2024-02-23 17:54:57 +0900

    fail to solve

--- SWEA/17.start_2/SWEA_1242.py
+++ SWEA/17.start_2/SWEA_1242.py
@@ -1,1 +1,16 @@
-print(bin(-128))
+T = int(input())
+
+for tc in range(1, T+1) :
+    n, m = map(int, input().split())
+
+    # 암호가 들어있는 줄만 추출, 겹치는 부분 삭제
+    code = []
+    for i in range(n) :
+        sub = list(map(str, input()))
+        if (sub != ['0'] * m) and (sub not in code) :
+            code.append(sub)
+    
+    for i in code :
+        i.reverse()
+
+    
