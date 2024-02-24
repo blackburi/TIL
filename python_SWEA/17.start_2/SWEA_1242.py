e665496e5e07481bd40266a9f60cb2638c7644e2
김태경 <98coding@gmail.com>
2024-02-25 00:39:13 +0900

    solve SWEA_1242.py

--- python_SWEA/17.start_2/SWEA_1242.py
+++ python_SWEA/17.start_2/SWEA_1242.py
@@ -1,26 +1,29 @@
 # 암호의 길이가 늘어나지 않았을때(56자리 일 때)
 main_dic = {
-    1011000 : 0,
-    1001100 : 1,
-    1100100 : 2,
-    1011110 : 3,
-    1100010 : 4,
-    1000110 : 5,
-    1111010 : 6,
-    1101110 : 7,
-    1110110 : 8,
-    1101000 : 9
+    '1011000' : 0,
+    '1001100' : 1,
+    '1100100' : 2,
+    '1011110' : 3,
+    '1100010' : 4,
+    '1000110' : 5,
+    '1111010' : 6,
+    '1101110' : 7,
+    '1110110' : 8,
+    '1101000' : 9
 }
 
 # 암호의 길이가 늘어났을 때(56의 배수 일 때)
 sub_dic = {}
 
-# 9까지만 하는 이유는 가로 길이의 최대가 500이기 때문에
-# 암호의 길이는 최대 9배까지 늘어날 수 있기 때문이다.
-# 500/56=8.92... 이기 때문에 안전하게 9까지 생각
-for i in range(1, 10) :
+# 9까지만 하는 이유는 가로 길이의 최대가 500*4이기 때문에
+# 암호의 길이는 최대 35배까지 늘어날 수 있기 때문이다.
+# 2000/56=35.7... 이기 때문에
+for i in range(2, 36) :
     for key in main_dic :
-
+        increase = ''
+        for one in key :
+            increase += one * i
+        sub_dic[increase] = main_dic[key]
 
 T = int(input())
 
@@ -33,66 +36,64 @@ for tc in range(1, T+1) :
         sub = list(map(str, input()))
         if (sub != ['0'] * m) and (sub not in code) :
             code.append(sub)
+
+    # 코드가 들어있는 부분을 2진수로 변환후 그 줄을 하나의 문자열로 생성
+    for i in range(len(code)) :
+        tmp = ''
+        for j in range(len(code[i])) :
+            tmp += format(int(code[i][j], 16), 'b').zfill(4)
+        code[i] = tmp[::-1]
+
+    # 코드를 확인하기 전 코드 8자리를 먼저 추출(중복되는 숫자를 없애기 위해서)
+    # 코드 8개 숫자를 담을 이중 list
+    check = []
     
     for i in code :
-        i.reverse()
+        # 코드의 시작점 체크
+        j = 0
+        while j < len(i) :
+            if i[j] != '0' :
+                # 코드(길이에 맞춰서)를 check에 넣어준다.
+                for q in range(1, 36) :
+                    if i[j:j+7*q] in main_dic or i[j:j+7*q] in sub_dic :
+                        if i[j:j+7*q] not in check :
+                            check.append(i[j:j+56*q])
+                            break
+                # j를 코드 길이만큼 뒤로 넘긴다.
+                j += 56*q
+            else :
+                j += 1
 
-"""
-decryption = {
-    '0001101':0,
-    '0011001':1,
-    '0010011':2,
-    '0111101':3,
-    '0100011':4,
-    '0110001':5,
-    '0101111':6,
-    '0111011':7,
-    '0110111':8,
-    '0001011':9
-}
+    # check에 있는 암호들을 8자리 숫자들로 바꾸고 올바른 암호라면 합을 ans에 담아준다.
+    ans = []
+    for i in check :
+        k = len(i)//56 # 7자리 암호코드가 늘어난 비율
+        if k == 1 :
+            lst = []
+            for x in range(8) :
+                lst.append(main_dic[i[7*x:7*x+7]])
+            lst.reverse()
+            if lst not in ans :
+                ans.append(lst)
+        else : # k >= 2
+            lst = []
+            for x in range(8) :
+                lst.append(sub_dic[i[7*x*k:7*x*k+7*k]])
+            lst.reverse()
+            if lst not in ans :
+                ans.append(lst)
 
-ch_len = {}
+    # ans에 있는 8자리 숫자들이 올바른 코드인지 확인한다
+    number = []
+    for i in ans :
+        if ((i[0] + i[2] + i[4] + i[6])*3 + i[1] + i[3] + i[5] + i[7]) % 10 == 0 :
+            hap = 0
+            for r in range(8) :
+                hap += i[r]
+            number.append(hap)
 
-for i in range(1, 36):
-    tmp = set()
-    for key in decryption:
-        tmp_str = ''
-        for letter in key:
-            tmp_str += letter * i
-        tmp.add(tmp_str)
-    ch_len[i] = tmp
-
-T = int(input())
-
-for test_case in range(1, T+1):
-    N, M = map(int, input().split())
-    codes = set()
-    ch = set()
-    for _ in range(N):
-        S = input().strip().strip('0')
-        if S and S not in ch:
-            ch.add(S)
-            S = bin(int(S, 16))[2:].rstrip('0')
-            while S:
-                for i in range(1, 36):
-                    if S[-7*i:] in ch_len[i]:
-                        break
-                codes.add((S[-56*i:].strip('0'), i))
-                S = S[:-56*i].rstrip('0')
-
-    total = 0
-    for code, cnt in codes:
-        code = code.zfill(cnt * 56)
-        length = len(code)
-        tmp1, tmp2 = 0, 0
-        for i in range(0, length, 7*cnt):
-            if i//cnt % 2 == 0:
-                tmp1 += decryption[code[i:i+7*cnt:cnt]]
-            else:
-                tmp2 += decryption[code[i:i+7*cnt:cnt]]
-
-        if (tmp1 * 3 + tmp2) % 10 == 0:
-            total += tmp1 + tmp2
-
-    print(f"#{test_case} {total}")
-"""
+    end = 0
+    for i in number :
+        end += i
+    
+    print(f'#{tc} {end}')
