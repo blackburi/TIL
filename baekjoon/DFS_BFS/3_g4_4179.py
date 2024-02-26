64e0772b2a1a41b53edcd6741be92ee56e5b50b7
김태경 <98coding@gmail.com>
2024-02-26 21:56:33 +0900

    fix explain

--- baekjoon/DFS_BFS/3_g4_4179.py
+++ baekjoon/DFS_BFS/3_g4_4179.py
@@ -9,29 +9,29 @@ mat = []
 time = 0
 
 # 지훈이와 불난 곳 저장할 곳
-F = deque()
-J = deque()
+fire = deque()
+jh = deque()
 
 # 지훈이와 불난곳 찾기
 for i in range(r):
     sub = list(input().rstrip())
     for j in range(c):
         if sub[j] == 'J':
-            J.append((i, j))
+            jh.append((i, j))
         if sub[j] == 'F':
-            F.append((i, j))
+            fire.append((i, j))
     mat.append(sub)
 
 
 def bfs():
-    global F, J, time
+    global fire, jh, time
 
     while True :
         time += 1
         tmp = []
         # 불이 번지는 것
-        while F:
-            x, y = F.popleft()
+        while fire:
+            x, y = fire.popleft()
             for i in range(4):
                 mx = x + dx[i]
                 my = y + dy[i]
@@ -39,12 +39,12 @@ def bfs():
                     if mat[mx][my] == '.' or mat[mx][my] == '/':
                         tmp.append((mx, my))
                         mat[mx][my] = 'F'
-        F = deque(tmp)
+        fire = deque(tmp)
 
         tmp = []
         # 지훈이가 도망가는 과정
-        while J:
-            x, y = J.popleft()
+        while jh:
+            x, y = jh.popleft()
             if x == 0 or y == 0 or x == r - 1 or y == c - 1:
                 return time
 
@@ -58,8 +58,8 @@ def bfs():
                     # 지훈이의 현재 위치
                     mat[mx][my] = 'J'
 
-        J = deque(tmp)
-        if not J:
+        jh = deque(tmp)
+        if not jh:
             return False
 
 
