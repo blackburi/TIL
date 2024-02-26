84f8b777fe3666dd4b96a33c09f3ebe1063497fa
김태경 <98coding@gmail.com>
2024-02-26 13:50:42 +0900

    fail to solve

--- baekjoon/DFS_BFS/3_g5_1068.py
+++ baekjoon/DFS_BFS/3_g5_1068.py
@@ -5,5 +5,13 @@ n = int(input())
 lst = list(map(int, input().rstrip().split()))
 m = int(input())
 
-cnt = lst.count(-1)
-print(n-cnt)
+sub = []
+for i in range(n) :
+    if lst[i] != -1 :
+        sub.append([i, lst[i]])
+
+for i in sub :
+    if m in i :
+        del i
+
+print(sub)
