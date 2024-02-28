62d810e3c3cd0f81edbbcffe423d08fcbe33161e
김태경_laptop <98coding.gmail.com>
2024-02-28 22:39:05 +0900

    solve SWEA_1244.py

--- python_SWEA/20.BF_greedy_2/SWEA_1244.py
+++ python_SWEA/20.BF_greedy_2/SWEA_1244.py
@@ -1,26 +1,75 @@
+# 최대 상금
+
 T = int(input())
 
 for tc in range(1, T+1) :
-    # b는 교환 횟수
-    a, b = map(int, input().split())
-    
-    # lst는 a를 한자리씩 끊어놓은 list
-    lst = list(str(a))
+    number, k = map(int, input().split())
+    lst = list(str(number))
     for i in range(len(lst)) :
         lst[i] = int(lst[i])
 
-    max_lst = sorted(lst)
-    max_lst.reverse()
-
-    while b :
-        if lst == max_lst :
-            break
-
-        for i in range(len(lst)) :
-            if lst[i] != max_lst[i] :
-                a = lst[-1:i:-1].index(max_lst[i])
-                lst[i], lst[i+len(lst[i+1:])-a] = lst[i+len(lst[i+1:])-a], lst[i]
-                b -= 1
-
-                if b == 0 :
+    # 숫자가 모두 다를 경우
+    if len(set(lst)) == len(lst) :
+        sub = sorted(lst)
+        sub.reverse()
+        while k :
+            for i in range(len(lst)) :
+                if lst[i] != sub[i] :
+                    a = lst.index(sub[i])
+                    lst[i], lst[a] = lst[a], lst[i]
+                    k -= 1
+                
+                if k == 0 :
                     break
+
+                if lst == sub:
+                    break
+            if lst == sub :
+                break
+        
+        while k :
+            lst[-1], lst[-2] = lst[-2], lst[-1]
+            k -= 1
+        
+        print(f'#{tc}', ' ', *lst, sep='')
+    
+    # 숫자가 같은것이 있는 경우
+    else :
+        sub = sorted(lst)
+        sub.reverse()
+        if k >= len(lst) :
+            print(f'#{tc}', ' ', *sub, sep='')
+        else : # k < len(lst)
+            while k :
+                # 똑같은 숫자가 몇개 있는지 담는 변수
+                tmp = 1
+                for i in range(len(lst)) :
+                    if lst[i] != sub[i] :
+                        a = lst[-1:i:-1].index(sub[i])
+                        lst[i], lst[-(a+1)] = lst[-(a+1)], lst[i]
+                        k -= 1
+                    
+                    if i >= 1 and lst[i] == lst[i-1] :
+                        tmp += 1
+                        if i == 1 :
+                            rev = lst[-i-tmp:]
+                            rev.sort(reverse = True)
+                            lst[-i-tmp:] = rev
+                        else :
+                            rev = lst[-i-tmp:-i+1]
+                            rev.sort(reverse = True)
+                            lst[-i-tmp:-i+1] = rev
+                    else :
+                        tmp = 1
+
+                    # 똑같은 숫자가 있는 경우 k가 남아도 같은 숫자끼리 자리를 바꾸면 된다.
+                    if lst == sub :
+                        break
+
+                    if k == 0 :
+                        break
+                
+                if lst == sub :
+                    break
+
+            print(f'#{tc}', ' ', *lst, sep='')
