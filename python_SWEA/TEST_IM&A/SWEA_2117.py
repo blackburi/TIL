d3b3805095140760ed845819bee03364950dad2f
김태경 <98coding@gmail.com>
2024-03-03 16:40:33 +0900

    fail to solve

---
+++ python_SWEA/TEST_IM&A/SWEA_2117.py
@@ -0,0 +1,14 @@
+# 홈 방범 서비스
+
+
+
+T = int(input())
+
+for tc in range(1, T+1) :
+    n, m = map(int, input().split())
+    mat = [list(map(str, input().split())) for _ in range(n)]
+
+    # 기업의 최대 이득
+    ans = 0
+
+    for k in range()
