45e482816fec8eea396ff1a4886299b0831a6762
김태경 <98coding@gmail.com>
2024-03-05 09:38:13 +0900

    solve level1_59403,sql

---
+++ SQL_programmers/01_select/level1_59403.sql
@@ -0,0 +1,5 @@
+-- 동물의 아이디와 이름
+
+select animal_id, name
+from animal_ins
+order by animal_id
--- baekjoon/DFS_BFS/3_g4_14502.py
+++ baekjoon/DFS_BFS/3_g4_14502.py
@@ -1,2 +1,18 @@
 # 연구소
 
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().rstrip().split())
+mat = [list(map(str, input().rstrip().split())) for _ in range(n)]
+
+
+
+# 안전 구역의 넓이
+ans = 0
+
+# 안전구역은 바이러스가 퍼진 이후 '0'의 영역
+for i in range(n) :
+    ans += mat[i].count('0')
+
+print(ans)
