feddb016f20d9e26653475b0bbdc432d25b9f3d6
김태경 <98coding@gmail.com>
2024-03-05 20:52:44 +0900

    solve 3_g4_14502.py

--- baekjoon/DFS_BFS/3_g4_14502.py
+++ baekjoon/DFS_BFS/3_g4_14502.py
@@ -2,36 +2,129 @@
 
 import sys
 input = sys.stdin.readline
+from collections import deque
 from itertools import combinations
+import copy
+
+n, m = map(int, input().split())
+mat = [list(map(str, input().rstrip().split())) for _ in range(n)]
+
+# 벽을 세울수 있는 list
+walls = []
+# virus가 있는 list
+q = []
+
+for i in range(n) :
+    for j in range(m) :
+        if mat[i][j] == '0' :
+            walls.append([i, j])
+
+        if mat[i][j] == '2' :
+            q.append([i, j])
+
+# 안전한 구역이 가장 넓을때의 변수
+safe_area = 0
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+def virus(matrix) :
+    global safe_area
+
+    tmp_q = deque(copy.deepcopy(q))
+    
+    while tmp_q :
+        x, y = tmp_q.popleft()
+        for i in range(4) :
+            mx = x + dx[i]
+            my = y + dy[i]
+            if 0 <= mx <= n-1 and 0 <= my <= m-1 and matrix[mx][my] == '0' :
+                matrix[mx][my] = '2'
+                tmp_q.append([mx, my])
+
+    # 안전구역 면적
+    safe = 0
+    for i in range(n) :
+        safe += matrix[i].count('0')
+    safe_area = max(safe_area, safe)
 
 
-def virus() :
+# 벽 3개 선택
+for wall in combinations(walls, 3) :
+    tmp_mat = copy.deepcopy(mat)
+    for x, y in wall :
+        tmp_mat[x][y] = '1'
+    virus(tmp_mat)
 
+print(safe_area)
+
+
+
+########################################################################
+# 시간초과
+########################################################################
+
+
+import sys
+input = sys.stdin.readline
+from collections import deque
+import copy
 
 
 n, m = map(int, input().rstrip().split())
 mat = [list(map(str, input().rstrip().split())) for _ in range(n)]
 
-# 전체 지역에서 1과 2가 아닌 곳(0인 곳)에서 3곳을 뽑아 벽을 세운후 count
-# 벽을 세울수 있는 통로 list
-lst = []
-for i in range(n) :
-    for j in range(n) :
-        if mat[i][j] == '0' :
-            lst.append([i, j])
+# 안전한 구역이 가장 넓을때의 변수
+safe_area = 0
 
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
-# 벽을 세울 3곳을 정하기a
-for a, b, c in combinations(lst, 3) :
-    mat[a[0]][a[1]] = '1'
-    mat[b[0]][b[1]] = '1'
-    mat[c[0]][c[1]] = '1'
+# 벽 3개를 선택
+def wall(cnt) :
+    if cnt == 3 :
+        virus()
+        return
+    
+    for i in range(n) :
+        for j in range(m) :
+            if mat[i][j] == '0' :
+                mat[i][j] = '1'
+                wall(cnt+1)
+                mat[i][j] = '0'
 
-# 안전 구역의 넓이
-ans = 0
+# wall()에서 벽을 세웠으면 virus()로 퍼뜨림
+def virus() :
+    global safe_area
 
-# 안전구역은 바이러스가 퍼진 이후 '0'의 영역
-for i in range(n) :
-    ans += mat[i].count('0')
+    q = deque()
 
-print(ans)
+    tmp_mat = copy.deepcopy(mat)
+
+    for i in range(n) :
+        for j in range(m) :
+            if tmp_mat[i][j] == '2' :
+                q.append([i, j])
+
+    while q :
+        x, y = q.popleft()
+
+        for i in range(4) :
+            mx = x+dx[i]
+            my = y+dy[i]
+
+            if 0 <= mx <= n-1 and 0 <= my <= m-1 :
+                if tmp_mat[mx][my] == '0' :
+                    tmp_mat[mx][my] = '2'
+                    q.append([mx, my])
+
+    tmp = 0
+    for i in range(n) :
+        for j in range(m) :
+            if tmp_mat[i][j] == '0' :
+                tmp += 1
+    
+    safe_area = max(safe_area, tmp)
+
+wall(0)
+print(safe_area)
