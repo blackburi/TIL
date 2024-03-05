4e063df24fe5b2b7baf577ae3defd7c573165230
김태경 <98coding@gmail.com>
2024-03-05 14:13:18 +0900

    fail to solve

--- baekjoon/DFS_BFS/3_g3_2146.py
+++ baekjoon/DFS_BFS/3_g3_2146.py
@@ -4,6 +4,10 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
+
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
+
+n = int(input())
+mat = [list(map(str, input().rstrip().split())) for _ in range(n)]
 
