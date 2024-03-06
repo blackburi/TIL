41dbd3fcc3578a7d0bbb6c12a657a6df0a85bd10
김태경 <98coding@gmail.com>
2024-03-07 00:03:11 +0900

    fail to solve

--- python_softeer/level3_6257.py
+++ python_softeer/level3_6257.py
@@ -1,1 +1,5 @@
 # 통근버스 출발 순서 검증하기
+
+import sys
+input = sys.stdin.readline
+
--- python_softeer/level3_6266.py
+++ python_softeer/level3_6266.py
@@ -1,4 +1,4 @@
-# 염기서열 커버
+# 회의실 예약
 
 import sys
 input = sys.stdin.readline
@@ -6,7 +6,27 @@ input = sys.stdin.readline
 # n 회의실 개수, m 회의 개수
 n, m = map(int, input().split())
 
-# 회의실 이름을 담는 list
-lst = []
-for _ in range(n) :
-    lst.append(input().rstrip())
+# {회의실 이름 : 시간}
+room = {}
+
+for i in range(n) :
+    name = input()
+    room[name] = [9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18,]
+
+# 회의실 이름순으로 정렬
+room = dict(sorted(room.items()))
+print(room)
+# 회의실 예약시간을 제외하는 함수
+def book(name, start, end) :
+    for i in range(start, end+1) :
+        if i in room[name] :
+            room[name].remove(i)
+
+# 예약한 정보와 시간을 받고 처리
+for _ in range(m) :
+    car, start, end = map(str, input().rstrip().split())
+    start = int(start)
+    end = int(end)
+    book(car, start, end)
+
+print(room)
