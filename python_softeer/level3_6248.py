1e900df61b182cbc4ebba30e34d063655d67206a
김태경 <98coding@gmail.com>
2024-03-12 12:24:39 +0900

    fail to solve

--- python_softeer/level3_6248.py
+++ python_softeer/level3_6248.py
@@ -1,2 +1,15 @@
 # 출퇴근길
 
+import sys
+input = sys.stdin.readline
+
+# 정점의 수, 간선의 수
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+for _ in range(m) :
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[a].sort()
+
+s, t = map(int, input().split())
