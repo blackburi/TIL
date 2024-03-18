73fbdd2c717f127030615d8480567daf00768803
김태경 <98coding@gmail.com>
2024-03-19 01:58:12 +0900

    solve SWEA_18639.py

--- python_SWEA/22.DC&backtracking/SWEA_18639.py
+++ python_SWEA/22.DC&backtracking/SWEA_18639.py
@@ -1,16 +1,38 @@
 # 병합 정렬
 
-def merge_sort(lst_left, lst_right) :
-    if len(lst_left) == 1 :
-        return lst_left
-    
-    if len(lst_right) == 1 :
-        return lst_right
+def merge_sort(lst) :
+    global cnt
 
+    if len(lst) == 1 :
+        return lst
     
+    l = len(lst)
+    left, right = [], []
+    middle = l // 2
 
+    for i in range(middle) :
+        left.append(lst[i])
+    
+    for j in range(middle, l) :
+        right.append(lst[j])
+    
+    left, right = merge_sort(left), merge_sort(right)
+
+    if left[-1] > right[-1] :
+        cnt += 1
+
+    re = left + right
+    re.sort()
+
+    return re
 
 
 T = int(input())
 
 for tc in range(1, T+1) :
+    n = int(input())
+    lst = list(map(int, input().split()))
+    cnt = 0
+    answer = merge_sort(lst)
+
+    print(f'#{tc} {answer[n//2]} {cnt}')
