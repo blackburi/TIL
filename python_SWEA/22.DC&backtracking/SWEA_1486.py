de8cb3d9d1362f5ed6a9e80a0c83d31fc85306ba
김태경 <98coding@gmail.com>
2024-03-18 23:47:01 +0900

    solve SWEA_1486.py

--- python_SWEA/22.DC&backtracking/SWEA_1486.py
+++ python_SWEA/22.DC&backtracking/SWEA_1486.py
@@ -1,39 +1,32 @@
 # 장훈이의 높은 선반
 
-def back(x) :
-    global gap, hap, b, n
-
-    if x == n :
-        if hap >= b :
-            gap = min(gap, hap-b)
-            print(hap)
-        return
-
+def dfs(x) :
+    global n, b, hap, ans
     if hap >= b :
-        gap = min(gap, hap-b)
-        print(hap)
+        ans = min(ans, hap - b)
         return
 
     for i in range(x, n) :
         hap += heights[i]
-        back(x+1)
+        dfs(i+1)
         hap -= heights[i]
+    
+    return
 
 
 T = int(input())
 
 for tc in range(1, T+1) :
-    # n 점원의 수, b 최소 높이
+    # 점원의 수, 선반의 높이
     n, b = map(int, input().split())
 
     heights = list(map(int, input().split()))
 
-    # 높이의 차(결과값)
-    gap = sum(heights)
+    # 점원들의 키의 합
+    hap = 0
+    # 정답
+    ans = sum(heights)
 
-    for _ in range(n) :
-        # 점원 키의합
-        hap = 0
-        back(0)
-    
-    print(gap)
+    dfs(0)
+
+    print(f'#{tc} {ans}')
