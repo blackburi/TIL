d9853f333fe9e2435902e53a2355725ea4eb4d15
김태경 <98coding@gmail.com>
2024-03-21 12:46:55 +0900

    solve SWEA_18647.py

--- python_SWEA/25.graph2/SWEA_18647.py
+++ python_SWEA/25.graph2/SWEA_18647.py
@@ -2,61 +2,56 @@
 
 from heapq import heappush, heappop
 
-def find_set(x) :
-    if parents[x] == x :
-        return x
-    
-    # 경로 압축
-    parents[x] = find_set(parents[x])
-    return parents[x]
 
+def dijkstra(start) :
+    pq = []
 
-def union(x, y) :
-    x = find_set(x)
-    y = find_set(y)
+    # 시적점의 노드와 cost를 저장
+    heappush(pq, (start, 0))
 
-    # 같은 집합이면 pass
-    if x == y :
-        return
-    
-    if x < y :
-        parents[y] = x
-    else :
-        parents[x] = y
+    # 시작 노드 초기화
+    distance[start] = 0
+
+    while pq :
+        # 최단 거리 노드에 대한 정보
+        now, dist = heappop(pq)
+
+        # 더 짧게 온 경우
+        if distance[now] < dist :
+            continue
+
+        # now에서 인접한 다른 노드 확인
+        for go in graph[now] :
+            next_dist = go[1]
+            next_node = go[0]
+
+            # 누적거리 계산
+            new_dist = dist + next_dist
+
+            # 이미 짧은 거리로 방문했다면 
+            if new_dist > distance[next_node] :
+                continue
+
+            # 누적거리 갱신
+            distance[next_node] = new_dist
+            # next_node의 인접 노드들을 pq에 추가
+            heappush(pq, (next_node, new_dist))
 
 
 T = int(input())
-
 for tc in range(1, T+1) :
     final, e = map(int, input().split())
+    inf = int(1e9)
 
-    graph = []
+    # 인접 list
+    graph = [[] for _ in range(final+1)]
+
+    # 누적거리 저장
+    distance = [inf] * (final+1)
 
     for _ in range(e) :
         start, end, cost = map(int, input().split())
-        graph.append((start, end, cost))
+        graph[start].append([end, cost])
 
-    graph.sort(key = lambda x:x[2])
-    parents = [i for i in range(final+1)]
-
-    # MST 완성 확인
-    cnt = 0
-
-    # 최소 비용
-    min_cost = 0
-
-    # 간선들 모두 확인
-    for start, end, cost in graph :
-        if find_set(start) == find_set(end) :
-            continue
-
-        cnt += 1
-
-        # cycle이 없다면 통과
-        union(start, end)
-        min_cost += cost
-
-        if cnt == final - 1 :
-            break
-    
-    print(f'#{tc} {min_cost}')
+    dijkstra(0)
+    print(f'#{tc} {distance[final]}')
