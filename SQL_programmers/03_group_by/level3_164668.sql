6d8469e2f57d769206ed12924c6616731f139f59
김태경 <98coding@gmail.com>
2024-03-21 23:51:25 +0900

    solve level3_164668.sql

--- SQL_programmers/03_group_by/level3_164668.sql
+++ SQL_programmers/03_group_by/level3_164668.sql
@@ -1,4 +1,8 @@
 -- 조건에 맞는 사용자와 총 거래금액 조회하기
 
-select user_id, nickname, (총거래 금액)
-from used_goods_user join used_goods_board
+select u.user_id, u.nickname, sum(price) as total_sales
+from used_goods_board b join used_goods_user u on b.writer_id = u.user_id
+where b.status = 'DONE'
+group by writer_id
+having total_sales >= 700000
+order by total_sales asc
