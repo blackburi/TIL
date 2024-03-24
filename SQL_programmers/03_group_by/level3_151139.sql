2dae76bec21d13450a114497f3fb4ca4ec03f969
김태경 <98coding@gmail.com>
2024-03-24 13:34:20 +0900

    solve level3_151139.sql

---
+++ SQL_programmers/03_group_by/level3_151139.sql
@@ -0,0 +1,13 @@
+-- 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기
+
+select month(start_date) as month, car_id, count(history_id) as records
+from car_rental_company_rental_history
+where car_id in (select car_id
+                 from car_rental_company_rental_history
+                 where date_format(start_date, '%Y-%m') between '2022-08' and '2022-10'
+                 group by car_id
+                 having count(car_id) >= 5) and
+      date_format(start_date, '%Y-%m') between '2022-08' and '2022-10'
+group by month, car_id
+having records > 0
+order by month asc, car_id desc
