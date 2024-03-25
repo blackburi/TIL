8a2800a2eb5d9d3cbddce5452c329cf49e247d7a
김태경 <98coding@gmail.com>
2024-03-25 11:41:09 +0900

    solve level2_133026.sql

--- SQL_programmers/03_group_by/level2_133026.sql
+++ SQL_programmers/03_group_by/level2_133026.sql
@@ -1,2 +1,6 @@
 -- 성분으로 구분한 아이스크림 총 주문량
 
+select ingredient_type, sum(total_order) as total_order
+from first_half join icecream_info on first_half.flavor = icecream_info.flavor
+group by icecream_info.ingredient_type
+order by sum(total_order) asc
