2dcb38abdd057a931d67a8ac4fefbec7704e7079
김태경 <98coding@gmail.com>
2024-03-27 21:05:08 +0900

    solve level3_144855.sql

--- SQL_programmers/03_group_by/level3_144855.sql
+++ SQL_programmers/03_group_by/level3_144855.sql
@@ -1,1 +1,7 @@
 -- 카테코리 별 도서 판매량 집계하기
+
+select category, sum(sales) as total_sales
+from book b join book_sales s using (book_id)
+where s.sales_date between "2022-01-01" and "2022-01-31"
+group by b.category
+order by category
