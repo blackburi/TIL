904fd14f98cd54e8e8a80e537baa728e4845cf9e
김태경 <98coding@gmail.com>
2024-03-28 22:36:25 +0900

    solve level3_157340.sql

--- SQL_programmers/03_group_by/level3_157340.sql
+++ SQL_programmers/03_group_by/level3_157340.sql
@@ -1,3 +1,13 @@
 -- 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기
 
-select 
+select car_id,
+        case
+        when car_id in (select car_id
+                        from car_rental_company_rental_history
+                        where '2022-10-16' between start_date and end_date)
+                        then '대여중'
+        else '대여 가능'
+        end as availability
+from car_rental_company_rental_history
+group by car_id
+order by car_id desc
