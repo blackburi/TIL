a4348f9ac3957b582a5f9aa7204a3ed06d2cd721
김태경 <98coding@gmail.com>
2024-04-01 20:49:09 +0900

    fail to solve

--- python_SWEA_study/SWEA_2112.py
+++ python_SWEA_study/SWEA_2112.py
@@ -1,31 +1,10 @@
 # 보호 필름
 
-import sys
-input = sys.stdin.readline
-import heapq
+T = int(input())
+for tc in range(1, T+1) :
+    # 보호 필름 두께, 가로 크기, 합격 기준
+    d, w, k = map(int, input().split())
 
-n, k = map(int, input().split())
-
-# 전체 보석
-jewels = []
-for _ in range(n) :
-    # (보석 무게, 보석 가격)
-    heapq.heappush(jewels, tuple(map(int, input().rstrip().split())))
-
-# 가방에 넣은 보석
-bags = []
-for _ in range(k) :
-    bags.append(int(input().rstrip()))
-
-total = 0
-jewel = []
-for bag in bags :
-    while jewels and bag >= jewels[0][0] :
-        heapq.heappush(jewel, -heapq.heappop(jewels)[1])
-    
-    if jewel :
-        total -= heapq.heappop(jewel)
-    elif not jewels :
-        break
-
-print(total)
+    film = []
+    for _ in range(d) :
+        film.append(list(map(int, input().split())))
--- python_SWEA_study/SWEA_2115.py
+++ python_SWEA_study/SWEA_2115.py
@@ -1,6 +1,42 @@
 # 벌꿀 채취
 
+from itertools import combinations
+
+# 꿀통 list를 가져왔을때 최대 수익을 내는 경우
+def comb(lst) :
+    re = 0
+    for i in range(1, m+1) :
+        # lst에서 i개를 선택
+        combine = list(combinations(lst, i))
+        for j in combine :
+            total = 0
+            if sum(j) <= c :
+                for k in j :
+                    total += k**2
+            re = max(re, total)
+    return re
+
+
+def find(box, total) :
+    global ans
+
+    # 일꾼들이 채집을 완료한 경우
+    if box == 2 :
+        ans = max(ans, total)
+        return
+
+    # 벌꿀 통을 선정
+
+
+
 T = int(input())
 for tc in range(1, T+1) :
-    n, m, c = map(int, input().rstrip().split())
-    
+    # 벌통의 크기, 선택할 수 있는 벌통의 수, 채취 가능한 꿀의 최대 양
+    n, m, c = map(int, input().split())
+    mat = [list(map(int, input().split())) for _ in range(n)]
+    visited = [[False]*n for _ in range(n)]
+
+    # 최대 이익
+    ans = 0
+
+https://tarra.tistory.com/entry/SW-Expert-Academy-2115-%EB%B2%8C%EA%BF%80%EC%B1%84%EC%B7%A8
