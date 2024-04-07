875f805cbf5b3ff0017c5fd20df422a0bcf320ae
김태경_laptop <98coding.gmail.com>
2024-04-07 21:44:05 +0900

    fail to solve

--- baekjoon/backtracking/3_g3_1941.py
+++ baekjoon/backtracking/3_g3_1941.py
@@ -1,2 +1,19 @@
 # 소문난 칠공주
 
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+def 
+
+mat = [list(input().rstrip()) for _ in range(5)]
+
+
+"""
+bfs 이후 dfs??
+
+하나로는 풀릴꺼 같지 않음
+되돌아갈 필요 없음 무조건 오른쪽 또는 아래 방향으로만 이동
+
+인원수가 바뀌면 바로 backtracking
+"""
