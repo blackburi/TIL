49c3cfb4862754f87e99a9d10bf5d6aed6e45508
김태경 <98coding@gmail.com>
2024-04-15 17:51:46 +0900

    fail to solve

--- baekjoon/backtracking/3_g3_1941.py
+++ baekjoon/backtracking/3_g3_1941.py
@@ -4,16 +4,11 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-def 
 
 mat = [list(input().rstrip()) for _ in range(5)]
 
+dx = [0, 1]
+dy = [1, 0]
 
-"""
-bfs 이후 dfs??
-
-하나로는 풀릴꺼 같지 않음
-되돌아갈 필요 없음 무조건 오른쪽 또는 아래 방향으로만 이동
-
-인원수가 바뀌면 바로 backtracking
-"""
+# 정답 수 출력
+ans = 0
