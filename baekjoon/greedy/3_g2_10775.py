237dbe57a001766b6b5b947b401a84bcb471c1c2
김태경 <98coding@gmail.com>
2024-04-16 16:22:01 +0900

    fail to solve

--- baekjoon/greedy/3_g2_10775.py
+++ baekjoon/greedy/3_g2_10775.py
@@ -5,8 +5,14 @@ input = sys.stdin.readline
 
 # 게이트의 수
 g = int(input())
+gate = [0]*g
 # 비행기의 수
 p = int(input())
 
-airplanes = [int(input()) for _ in range(p)]
-
+for _ in range(p) :
+    k = int(input().rstrip())
+    if gate[k] == 0 :
+        gate[k] == 1
+    else : # gate[k] == 1
+        while True :
+            
---
+++ baekjoon/greedy/4_p4_1422.py
@@ -0,0 +1,7 @@
+# 숫자의 신
+
+import sys
+input = sys.stdin.readline
+
+# k개의 수중 n개를 뽑아 사용
+k, n = map(int, input().split())
