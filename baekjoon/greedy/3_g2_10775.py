83dc3f4416819c9e46507f71d83de79f5d1875c8
김태경 <98coding@gmail.com>
2024-04-17 17:37:50 +0900

    fail to solve

--- baekjoon/greedy/3_g2_10775.py
+++ baekjoon/greedy/3_g2_10775.py
@@ -9,10 +9,24 @@ gate = [0]*g
 # 비행기의 수
 p = int(input())
 
+# 도킹할 수 있는 비행기를 넣는 곳
+maxplane = set()
+
 for _ in range(p) :
-    k = int(input().rstrip())
-    if gate[k] == 0 :
-        gate[k] == 1
-    else : # gate[k] == 1
-        while True :
-            
+    k = int(input())
+    if k not in maxplane :
+        maxplane.add(k)
+        continue
+    # k in maxplane
+    while k > 0 :
+        k -= 1
+        if k == 0 :
+            break
+        if k not in maxplane :
+            maxplane.add(k)
+            break
+    
+    if k == 0 :
+        break
+
+print(len(maxplane))
