c48c50c6d7399639282a49ead292fc7249aea178
김태경 <98coding@gmail.com>
2024-04-28 22:49:55 +0900

    solve SWEA_1949.py

--- python_SWEA_study/SWEA_1949.py
+++ python_SWEA_study/SWEA_1949.py
@@ -1,61 +1,57 @@
 # 등산로 조정
-
-
-# 현재 높이, 현재 등산로의 길이, 공사 횟수(max = 1)
-# 현재 위치 x, y
-
+from pprint import pprint
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
 
-# 현재 높이, 등산로 길이, 깎았는지, 현재 위치
-def dfs(height, leng, cut, x, y) :
-    global length
+def dfs(x, y, cut) :
+    global length, visited
 
-    length = max(length, leng)
+    length = max(length, visited[x][y])
 
     for i in range(4) :
         mx = x + dx[i]
         my = y + dy[i]
-        if 0 <= mx < n and 0 <= my < n and (visited[mx][my] is False) :
-            # 다음 이동이 현재보다 높이가 낮은 경우
-            if height > mat[mx][my] :
-                visited[mx][my] = True
-                dfs(mat[mx][my], leng+1, cut, mx, my)
-                visited[mx][my] = False
-            
-            # 다음 이동이 현재보다 높이가 같거나 높은 경우
-            if height <= mat[mx][my] and cut == 0 and mat[mx][my]-k < height :
-                visited[mx][my] = True
-                dfs(height-1, leng+1, cut+1, mx, my)
-                visited[mx][my] = False
+        if 0 <= mx < n and 0 <= my < n and visited[mx][my] == 0 :
+            if mat[x][y] > mat[mx][my] :
+                visited[mx][my] = visited[x][y] + 1
+                dfs(mx, my, cut)
+                visited[mx][my] = 0
+            elif cut == 0 and mat[mx][my] - k < mat[x][y] :
+                tmp = mat[mx][my]
+                mat[mx][my] = mat[x][y] - 1
+                visited[mx][my] = visited[x][y] + 1
+                dfs(mx, my, cut+1)
+                visited[mx][my] = 0
+                mat[mx][my] = tmp
 
 
 T = int(input())
 for tc in range(1, T+1) :
     n, k = map(int, input().split())
 
-    # 높이의 최댓값
-    max_height = 0
-    # 높이가 최대인 지점들의 집합
-    start = []
-
+    # 지도
     mat = []
+
+    heights = []
+    height = 0
     for i in range(n) :
-        sub = list(map(int, input().rstrip().split()))
+        sub = list(map(int, input().split()))
         for j in range(n) :
-            if sub[j] > max_height :
-                max_height = sub[j]
-                start = [(i, j)]
-            elif sub[j] == max_height :
-                start.append((i, j))
+            if sub[j] > height :
+                height = sub[j]
+                heights = [(i, j)]
+            elif sub[j] == height :
+                heights.append((i, j))
         mat.append(sub)
 
     # 최대 길이 등산로
     length = 0
-    # 방문체크
-    visited = [[False]*n for _ in range(n)]
+    # 방문처리
+    visited = [[0]*n for _ in range(n)]
 
-    for (a, b) in start :
-        dfs(max_height, 1, 0, a, b)
-
+    for (i, j) in heights :
+        visited[i][j] = 1
+        dfs(i, j, 0)
+        visited[i][j] = 0
+    
     print(f'#{tc} {length}')
