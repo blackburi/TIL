e409f072da395859d41985cea4cfbed123502b00
김태경 <98coding@gmail.com>
2024-08-19 13:36:45 +0900

    solve : have to check one more

--- baekjoon/greedy/3_g1_3430.py
+++ baekjoon/greedy/3_g1_3430.py
@@ -2,35 +2,65 @@
 
 import sys
 input = sys.stdin.readline
+import heapq
 
-z = int(input())
 
-for _ in range(z) :
-    # 호수, 비가 내리는 날의 수
-    n, m = map(int, input().split())
-    # 비가 오는 날 / 오지 않는 날을 list로 받기
+tc = int(input())
+for _ in range(tc) :
+    # 호수, 호수에 비가 내리는 날의 수
+    n, m = map(int, input().rstrip().split())
     rain = list(map(int, input().rstrip().split()))
 
-    # 마시는 물을 저장
-    drink = dict()
-    # 마셔야 하는 날의 수를 check
-    days = 0
-    # 불가능한지 가능한지를 체크하는 변수
-    flag = 0
+    # 현재 호수가 비었는지 확인하는 list
+    check = [False] * (n+1)
+    # 재앙을 피할수 있는지 확인하는 변수
+    flag = True
+    # 현재 호수 번호가 i일 때, 비가 오는 시간을 역순으로 저장
+    rev = [[] for _ in range(n+1)]
+    # 현재 시간 t에서 비가 올 때 해당하는 호수의 번호
+    time = [-1] * m
 
-    while rain :
-        day = rain.pop()
+    # 거꾸로 저장하여 먼저 마셔야 하는 호수의 번호를 우선순위로 마시도록 하기 위해서
+    for i in range(m-1, -1, -1) :
+        if rain[i] :
+            time[i] = rain[i]
+            rev[rain[i]].append(i)
 
-        # 비가 오는 날인 경우
-        if day != 0 :
-            # 같은 호수에 연속으로 2번 비가 오는경우
-            if drink[day] :
-                flag = 1
+    # 우선순위 큐에 삽입
+    pq = []
+    for i in range(1, n+1) :
+        if rev[i] :
+            heapq.heappush(pq, rev[i].pop())
+
+    # 용이 마시는 호수 번호를 저장하는 리스트
+    ans = []
+    for i in range(m) :
+        # 현재 비가 온다면
+        if rain[i] :
+            # 비가 오는데 호수가 비지 않았다면 -> 재앙이 일어남
+            if not check[rain[i]] :
+                flag = False
                 break
-            # 같은 호수에 아직 비가 오지 않은 경우
+            # 비가 오는데 호수가 비었다면
+            # 그 다음 비오는 시간을 우선순위 큐에 저장
+            check[rain[i]] = False
+            if rev[rain[i]] :
+                heapq.heappush(pq, rev[rain[i]].pop())
+        # 현재 비가 오지 않는다면
+        else :
+            # 마실수 있는 호수가 있다면 greedy로 마신다
+            if pq :
+                now = pq[0]
+                heapq.heappop(pq)
+                ans.append(time[now])
+                check[time[now]] = True
+            # 마실수 있는 호수가 없는 경우
             else :
-                drint[day] = 1
-                days += 1
-        # 비가 오지 않는 경우
-        else : # day == 0
-            days -= 1
+                ans.append(0)
+
+    # 정답 출력
+    if not flag :
+        print("NO")
+    else :
+        print("YES")
+        print(' '.join(map(str, ans)))
